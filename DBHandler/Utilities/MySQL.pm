650ca209761cadfa8b237fa6eb7c760edfb16a5e
f-myself <freaking.yourself@gmail.com>
2019-05-22 21:16:16 +0300

    Adding MySQL Handler

---
+++ DBHandler/Utilities/MySQL.pm
@@ -0,0 +1,85 @@
+#!/usr/bin/perl -w
+
+package Utilities::MySQL;
+
+use strict;
+use DBI;
+use Data::Dumper;
+
+
+sub new
+{
+    my $class = ref($_[0]) || $_[0];
+    my $self = {};
+    my $host = 'dbi:mysql:host=127.0.0.1;dbname=user6;';
+    my $user = 'user6';
+    my $pass = 'user6';
+    $self->{'dbh'} = DBI->connect($host, $user, $pass, {});
+    print "Package Initialized\n";
+    return bless($self, $class);
+}
+
+sub select
+{
+    my ($self, $what, $tableName, $where) = @_;
+    my $statmentString = 'SELECT ' . $what . ' FROM ' . $tableName;
+    if ($where)
+    {
+        $statmentString .= " WHERE " . $where;
+    }
+    my $sth = $self->{'dbh'}->prepare($statmentString);
+    my $cnt = $sth->execute();
+    my @data_array;
+
+    while (my $row=$sth->fetchrow_hashref())
+    {
+        push @data_array, $row;
+    }
+    $sth->finish();
+    return @data_array;
+}
+
+sub insert
+{
+    my ($self, $tableName, $fields, $values) = @_;
+    my $statmentString = 'INSERT INTO ' . $tableName 
+                                        . ' (' . $fields . ') VALUES (' 
+                                        . $values . ')';
+    if ($self->{'dbh'}->do($statmentString))
+    {
+        return "Inserted successfully\n";
+    }
+    return "Not inserted\n";
+}
+
+sub delete
+{
+    my ($self, $tableName, $conditions) = @_;
+    my $statmentString = 'DELETE FROM ' . $tableName . ' WHERE ' . $conditions;
+    if ($self->{'dbh'}->do($statmentString))
+    {
+        return "Deleted successfully\n";
+    }
+    return "Not deleted\n";
+}
+
+sub update
+{
+    my ($self, $tableName, $toChange, $conditions) = @_;
+    my $statmentString = 'UPDATE ' . $tableName . ' SET ' . $toChange . 'WHERE ' . $conditions;
+    if ($self->{'dbh'}->do($statmentString))
+    {
+        return "Updated successfully\n";
+    }
+    return "Not updated\n";
+}
+
+
+
+sub DESTROY
+{
+    my ($self) = @_;
+    $self->{'dbh'}->disconnect;
+}
+
+return 1;
